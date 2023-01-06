09bf8b805f8a263ddcdb45cdc48497765077eaff
evanalifian <evanalifian@gmail.com>
2023-01-06 21:49:08 +0700

    membuat repositori 'personal-website'

---
+++ script.js
@@ -0,0 +1,12 @@
+const menuToggle = document.querySelector('.navbar-menu-toggle');
+const social = document.querySelector('.navbar-list');
+
+menuToggle.addEventListener('click', () => {
+  menuToggle.classList.toggle('rotate');
+  social.classList.toggle('slide');
+});
+
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+  menuToggle.classList.remove('rotate');
+  social.classList.remove('slide');
+}));
